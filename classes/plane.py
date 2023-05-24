1fca5476743bd18d0653c919dbbc79f8dd2e204f
Snourry <30350968+Existrace@users.noreply.github.com>
2023-05-24 15:51:14 +0200

    Add Initialise seat map in Plane and children

--- classes/plane.py
+++ classes/plane.py
@@ -1,17 +1,35 @@
-class Plane:
+from abc import ABC
+
+
+class Plane(ABC):
     def __init__(self, registration_number, capacity):
         self.registration_number = registration_number
         self.capacity = capacity
         self.seat_map = {}
 
+    def initialize_seat_map(self, rows, seats_per_row):
+        for row in range(1, rows + 1):
+            for seat_index in range(1, seats_per_row + 1):
+                seat_letter = chr(ord("A") + seat_index - 1)
+                seat_number = f"{row}{seat_letter}"
+                self.seat_map[seat_number] = None
+
 
 class AirbusA320(Plane):
     def __init__(self, registration_number):
-        super().__init__(registration_number, capacity=174)
+        row = 20
+        seats_per_row = 6
+        capacity = row * seats_per_row
+        super().__init__(registration_number, capacity=capacity)
         self.registration_number = registration_number
+        super().initialize_seat_map(row, seats_per_row)
 
 
 class AirbusA220(Plane):
     def __init__(self, registration_number):
-        super().__init__(registration_number, capacity=148)
+        row = 30
+        seats_per_row = 6
+        capacity = row * seats_per_row
+        super().__init__(registration_number, capacity=capacity)
         self.registration_number = registration_number
+        super().initialize_seat_map(row, seats_per_row)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from classes.plane import AirbusA320
+from classes.plane import AirbusA220
 
 # from classes.passenger import Passenger
 
 
 def main():
-    airbusA220 = AirbusA320("F-WWBQ")
-    print(
-        f"A plane is available : {airbusA220.registration_number}"
-    )
+    airbusA220 = AirbusA220("F-WWBQ")
+    print(f"A plane is available : {airbusA220.registration_number}")
+    print(airbusA220.seat_map)
 
 
 if __name__ == "__main__":
