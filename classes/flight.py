985b3eb988ea7e9b262f1fe024bef0b186c1f779
Snourry <30350968+Existrace@users.noreply.github.com>
2023-05-25 10:38:01 +0200

    Add typing + refacto comment + add multi assign seat

--- classes/flight.py
+++ classes/flight.py
@@ -1,30 +1,42 @@
 import textwrap
+from typing import List
+from classes.passenger import Passenger
 
 
 class Flight:
-    def __init__(self, flight_number, plane):
-        self.flight_number = flight_number
+    def __init__(self, number, plane):
+        self.number = number
         self.plane = plane
         self.passengers = []
 
-    def assign_seat(self, passenger):
+    def assign_seat(self, passengers: List[Passenger]):
         """
         Assigns a seat to a passenger on the flight.
 
         Args:
-            passenger (Passenger): The passenger object for seat assignment.
+            passengers (List[Passenger]): A list of Passenger objects.
 
         Returns:
             bool: True if the seat was successfully assigned, False otherwise.
         """
-        available_seats = self.get_avalaible_seats()
-        if available_seats:
-            seat = available_seats[0]
-            self.plane.seat_map[seat] = passenger
-            passenger.seat = seat
-            return True
-        else:
-            return False
+        for passenger in passengers:
+            available_seats = self.get_avalaible_seats()
+            if available_seats:
+                seat = available_seats[0]
+                self.plane.seat_map[seat] = passenger
+                passenger.seat = seat
+            else:
+                return False
+        return True
+
+    # def reassign_seat(self, passengers: List[Passenger]):
+    #     # TODO
+    #     for passenger in passengers:
+    #         if passenger.seat is None:
+    #             print("There is no already seat attribute for {passenger.name}")
+    #             self.assign_seat(passenger)
+    #             print("Seat is attribute to {passenger.name} : {passenger.seat}")
+    #             return True
 
     def get_avalaible_seats(self):
         """
@@ -37,17 +49,27 @@ class Flight:
         for seat, passenger in self.plane.seat_map.items():
             if passenger is None:
                 available_seats.append(seat)
+        if not available_seats:
+            return False
         return available_seats
 
-    def show_reservation_details(self, passengers):
+    def show_reservation_details(self, passengers: List[Passenger]):
+        """
+        Displays the reservation details for one or more passengers.
+        Args:
+            passengers (List[Passenger]): A list of Passenger objects.
+        """
         for passenger in passengers:
-            details = textwrap.dedent(
-                f"""
-                Reservation details :
-                The flight number is {self.flight_number}
-                and the plane is {self.plane.registration_number}.
-                The passenger is {passenger.name}
-                Their seat is {passenger.seat}
-                """
-            )
-            print(details)
+            if passenger.seat:
+                details = textwrap.dedent(
+                    f"""
+                    Reservation details :
+                    The flight number is {self.number}
+                    and the plane is {self.plane.registration_number}.
+                    The passenger is {passenger.name}
+                    Their seat is {passenger.seat}
+                    """
+                )
+                print(details)
+            else:
+                print(f"{passenger.name} has not seat")
--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from classes.passenger import Passenger
 
 def main():
     # Declare one passenger
-    passenger_one = Passenger("Jones")
+    passenger_one = Passenger("Baghera")
+    passenger_two = Passenger("Jones")
     # Declare plane
     airbusA220 = AirbusA220("F-WWBQ")
     # Declare flight with previous plane declared
     flight = Flight("AF123", airbusA220)
 
     # Try to assign seat for passenger
-    flight.assign_seat(passenger_one)
-    flight.show_reservation_details([passenger_one])
+    flight.assign_seat([passenger_one, passenger_two])
+    flight.show_reservation_details([passenger_one, passenger_two])
+    # CA NE LIBERE PAS LE SIEGE QUAND J'ATTRIBUE COMME CA
+    # flight.assign_seat(passenger_one)
+    # flight.show_reservation_details([passenger_one])
+    print(flight.get_avalaible_seats())
 
 
 if __name__ == "__main__":
